9f7ad11f977144192afcda4793aa365523d8d75b
orjuela <julie.orjuela@ird.fr>
2019-07-25 13:24:11 +0200

    updating buildingBedFromDEandGff.py

--- buildingBedFromDEandGff.py
+++ buildingBedFromDEandGff.py
@@ -9,8 +9,8 @@
     ==========================
     :author:  Julie Orjuela
     :contact: julie.orjuela@ird.fr
-    :date: 28/10/2018
-    :version: 0.1
+    :date: 27/05/2019
+    :version: 0.4
     Script description
     ------------------
     buildingBedFromDEandGff create a bed containing UTRs regions from DE list genes using annotation gtf obtained by stringtie. 
@@ -52,8 +52,9 @@ from pprint import pprint
 ##################################################
 ## Variables Globales
 ##################################################
-version="0.1"
+version="0.4"
 VERSION_DATE='27/05/2019'
+last='24/07/2019'
 debug="False"
 #debug="True"
 
@@ -78,6 +79,14 @@ def relativeToAbsolutePath(relative):
         absolutePath = relative;
         return absolutePath
 
+def extract_chr_len(contig, fasta):
+    if contig is not None:
+        commande=f"grep {contig}[[:space:]] {fasta}.fai | cut -f 2"
+        #print(commande)
+        process = subprocess.run(commande,stdout=subprocess.PIPE,shell=True, encoding="utf-8") #utiliser la fontion decoding pour passer des b to utf-8"
+        return int(process.stdout)
+ 
+
 ##################################################
 ## Main code
 ##################################################
@@ -85,14 +94,16 @@ if __name__ == "__main__":
     # Initializations
     start_time = strftime("%d-%m-%Y_%H:%M:%S", localtime())
     # Parameters recovery
-    parser = argparse.ArgumentParser(prog='buildingBedFromDEandGff.py', description='''This Programme buildingBedFromDEandGff create a bed containing UTRs regions from DE list genes using annotation gtf obtained by stringtie''')
+    parser = argparse.ArgumentParser(prog='buildingBedFromDEandGff.py', description='''This Programme buildingBedFromDEandGff create a bed containing UTRs regions from DE list genes using annotation gtf obtained by stringtie, it extracts fasta sequences from bed file''')
     parser.add_argument('-v', '--version', action='version', version='You are using %(prog)s version: ' + version, help=\
                         'display buildingBedFromDEandGff.py version number and exit')
     filesreq = parser.add_argument_group('Input mandatory infos for running')
     filesreq.add_argument('-d', '--de', metavar="<filename>", required=True, dest = 'de_file', help = 'DE file')
     filesreq.add_argument('-g', '--gff', metavar="<filename>", required=True, dest = 'gff_file', help = 'gtf stringtie or gffcompare file')
+    filesreq.add_argument('-f', '--fasta', metavar="<filename>", required=True, dest = 'fasta_file', help = 'Name of fasta referece file')
     filesreq.add_argument('-o', '--out', metavar="<filename>", required=True, dest = 'bed_file', help = 'Name of output bed file')
 
+    
     # Check parameters
     args = parser.parse_args()
     checkParameters(args)
@@ -107,7 +118,16 @@ if __name__ == "__main__":
     de = relativeToAbsolutePath(args.de_file)
     gff = relativeToAbsolutePath(args.gff_file)
     bed = relativeToAbsolutePath(args.bed_file)
-
+    fasta = relativeToAbsolutePath(args.fasta_file)
+    fasta_extracted_file = bed.replace('.bed', '.fasta')
+    
+        
+    #indexation de la ref fasta
+    commande=f"samtools faidx {fasta}"
+    print(commande)
+    process = subprocess.run(commande,stdout=subprocess.PIPE,shell=True)
+    #print('----------------', process.stdout)
+    
     # parcourir de file et gff file au meme temps
     with open(de, "r") as de_file, open(bed, "w") as bed_out:
         linebed=["#contig","start","stop","name","score","strand"]
@@ -116,26 +136,54 @@ if __name__ == "__main__":
         # boucle sur le fichier de
         for lineD in de_file:
             dline=lineD.strip().split(",")
+            #print (dline)
             # on parcour le gff
-            count=0
+            #count=0
             with open(gff, "r") as gff_file:
-                for line in gff_file:
-                    en_tete_gff = gff_file.readline().rstrip()
+                en_tete_gff = gff_file.readline().rstrip()
+                for line in gff_file:    
                     if not line.startswith('#'):
                         gline=line.strip().split("\t")
-                        dline_cleaned=str(dline[0]).replace('\"','')
-                        if ('transcript' in gline[2]) and (dline_cleaned in gline[8]) and (count <3):
-                            print(dline[0],"-----------",gline[8], "------ count:", count)
-                            count=count+1
-                            start=int(gline[3])-1000
-                            if start<=0:
-                                start=1
-                            else:
-                                linebed=[gline[0],str(start),gline[3],gline[2],gline[5],gline[6]]
-                                bed_out.write("\t".join(linebed) + "\n")
+                        dline_cleaned=str(dline[0]).replace('\"','').strip()
+                        #print (dline_cleaned)
+                        #gene_id "MSTRG.3388|
+                        motif=f"^gene_id \"{dline_cleaned}(\||\";)" # attention Sans annot âs de pipe
+                        if ("transcript" in gline[2]):
+                            res=re.search(motif,gline[8])
+                            if res is not None:
+                                #print (res)
+                                #(dline_cleaned in gline[8]):
+                                #print ('>>>>',gline[2], gline[8])
+                                #print(dline[0],"-----------",gline[8], "------ count:", count)
+                                if (gline[6]=="+"):
+                                    start=int(gline[3])-1000
+                                    stop=int(gline[3])
+                                    if start<=0:
+                                        start=1
+                                    else:
+                                        linebed=[gline[0],str(start),str(stop),gline[2],gline[5],gline[6],dline_cleaned,gline[3],gline[4]]
+                                        print (linebed)
+                                        bed_out.write("\t".join(linebed) + "\n")
+                                if (gline[6]=="-"):
+                                    chrlen=extract_chr_len(gline[0],fasta)
+                                    start=int(gline[4])
+                                    stop=int(gline[4])+1000
+                                    if stop>chrlen:
+                                        start=chrlen
+                                    else:
+                                        linebed=[gline[0],str(start),str(stop),gline[2],gline[5],gline[6],dline_cleaned,gline[3],gline[4]]
+                                        print (linebed)
+                                        bed_out.write("\t".join(linebed) + "\n")
+                                #count=count+1        
     
-    #### faire un dico avec les sites trouvés apres avoir parcouri les csv d'un dossier donnée en paramettre.
-
+    # TODO changer le grep systeme pour une contruction d'un dico avec la taille de c/scaffold
+    
+    # extraction de sequence fasta avec getfasta en utilisant bed_out
+    commande=f"/home/orjuela/Documents/tools/bedtools2/bin/bedtools getfasta -fi {fasta} -bed {bed} > {fasta_extracted_file}"
+    print(commande)
+    process = subprocess.run(commande,stdout=subprocess.PIPE,shell=True, encoding="utf-8")
+    #print('----------------', process.stdout)
+    
     print("\nStop time: ", strftime("%d-%m-%Y_%H:%M:%S", localtime()))
     print("#################################################################")
     print("#                        End of execution                       #")
